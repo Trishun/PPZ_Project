7e98ceca402b1b63d12fa6a4392528aeb92bd557
Piotr Dec <piotr_dec@msn.com>
2017-04-06 10:25:32 +0200

    Lobbys creation (feature, involves#4)

--- src/Server/Player.java
+++ src/Server/Player.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * PPZ
  * Created by PD on 05.04.2017.
  */
-public class TaskHandler extends Thread{
+public class Player extends Thread{
 
     private BufferedReader socketReader;
     private PrintWriter socketWriter;
     private DatabaseCommunicator databaseCommunicator;
+    private Server up;
+    private int lobby;
 
     /**
      * Class constructor
@@ -22,10 +25,11 @@ public class TaskHandler extends Thread{
      * @param databaseCommunicator DatabaseCommunicator to be used.
      * @throws IOException
      */
-    TaskHandler(Socket clientSocket, DatabaseCommunicator databaseCommunicator) throws IOException {
+    Player(Socket clientSocket, DatabaseCommunicator databaseCommunicator, Server up) throws IOException {
         this.databaseCommunicator = databaseCommunicator;
         this.socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         this.socketWriter = new PrintWriter(clientSocket.getOutputStream());
+        this.up = up;
     }
 
     /** TODO
@@ -33,6 +37,22 @@ public class TaskHandler extends Thread{
      */
     public void run() {
 
+    }
+
+    public ArrayList<Player> refreshViewData() {
+        return up.refreshViewData(this);
+    }
+
+    private void joinLobby(int enterCode) {
+        up.addToLobby(enterCode, this);
+    }
+
+    public void setLobby (int lobby) {
+        this.lobby = lobby;
+    }
+
+    public int getLobby() {
+        return lobby;
     }
 
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -10,7 +10,8 @@ import java.util.ArrayList;
  */
 public class Server {
 
-    private static ArrayList<TaskHandler> taskHandlerList = new ArrayList<TaskHandler>();
+    private static ArrayList<Lobby> lobbyList = new ArrayList<Lobby>();
+    private static ArrayList<Player> playerList = new ArrayList<Player>();
     private DatabaseCommunicator databaseCommunicator;
 
     /**
@@ -22,10 +23,10 @@ public class Server {
     }
 
     void Run() {
-        new Thread(() -> mainTask()).start();
+        new Thread(() -> incoming()).start();
     }
 
-    void mainTask() {
+    private void incoming() {
         try {
             ServerSocket serverSocket = new ServerSocket(80);
 
@@ -36,13 +37,90 @@ public class Server {
                 System.out.println("Połączono!");
 
                 //Create thread to handle connection
-                TaskHandler taskHandler = new TaskHandler(socket, databaseCommunicator);
-                taskHandlerList.add(taskHandler);
-                taskHandler.start();
+                Player player = new Player(socket, databaseCommunicator, this);
+                playerList.add(player);
+                player.start();
             }
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    public void createLobby(Player initiator) {
+        Lobby lobby = new Lobby(lobbyList.size(), initiator);
+        lobbyList.add(lobby);
+        refreshViewData(lobby);
+    }
+
+    public void addToLobby(int enterCode, Player player) {
+        for (Lobby lobby : lobbyList) {
+            if (enterCode == lobby.getEnterCode()) {
+                addToLobby(lobby, player);
+                return;
+            }
+        }
+    }
+
+    public void addToLobby(Lobby lobby, Player player) {
+        lobby.addToLobby(player);
+        player.setLobby(lobby.getId());
+        refreshViewData(lobby);
+    }
+
+    public void refreshViewData(Lobby lobby) {
+        for (Player player : lobby.getLobbyList()) {
+            player.refreshViewData();
+        }
+    }
+    public ArrayList<Player> refreshViewData(Player player) {
+        return getLobbyById(player.getLobby()).getLobbyList();
+    }
+
+    private Lobby getLobbyById(int id) {
+        for (Lobby lobby : lobbyList) {
+            if (lobby.getId() == id) {
+                return lobby;
+            }
+        }
+        return null;
+    }
+
+}
+
+class Lobby {
+    private int id;
+    private Player initiator;
+    private ArrayList<Player> lobbyList = new ArrayList<Player>();
+    private int enterCode;
+
+    Lobby(int id, Player initiator) {
+        this.id = id;
+        this.initiator = initiator;
+        enterCode = enterCodeGeneration();
+        lobbyList.add(initiator);
+    }
+
+    private int enterCodeGeneration() {
+        return initiator.hashCode();
+    }
+
+    public int getEnterCode() {
+        return enterCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addToLobby(Player newPlayer) {
+        lobbyList.add(newPlayer);
+    }
+
+    public void removeFromLobby(Player player) {
+        lobbyList.remove(player);
+    }
+
+    public ArrayList<Player> getLobbyList() {
+        return lobbyList;
+    }
 }
