f38fefd304fd25695a9e08dcf201478f8f1d030d
Piotr Dec <piotr_dec@msn.com>
2017-04-23 21:58:39 +0200

    WIP (enhancement, involves #25)

---
+++ src/Server/src/Team.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Team main class
+ * Created by PD on 19.04.2017.
+ */
+class Team {
+
+    private int role;
+    private ArrayList<Player> players = new ArrayList<>();
+
+    void addPlayer(Player player) {
+
+    }
+
+    void removePlayer(Player player) {
+
+    }
+
+    int numberOfPlayers() {
+        return players.size();
+    }
+
+
+}
