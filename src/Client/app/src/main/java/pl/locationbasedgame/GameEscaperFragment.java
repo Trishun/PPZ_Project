f0ae3852c9cc2e36a90b6a26f535a1cebd4f5026
pligenz <ligenzapatryk@o2.pl>
2017-06-11 11:28:43 +0200

    Add listening for location (feature, involves #65)

--- src/Client/app/src/main/java/pl/locationbasedgame/GameEscaperFragment.java
+++ src/Client/app/src/main/java/pl/locationbasedgame/GameEscaperFragment.java
@@ -1,27 +1,71 @@
 package pl.locationbasedgame;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GameEscaperFragment extends Fragment {
 
+    private LocationManager locationManager;
+    private Location currentLocation;
+    private LocationListener locationListener;
 
     public GameEscaperFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                currentLocation = location;
+                Log.i("CURRENT LOC", currentLocation.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+            @Override
+            public void onProviderEnabled(String provider) { }
+
+            @Override
+            public void onProviderDisabled(String provider) { }
+        };
+
         return inflater.inflate(R.layout.fragment_game_escaper, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 5, locationListener);
+        } catch (SecurityException e) {
+            //catched when permissions are not granted
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        locationManager.removeUpdates(locationListener);
+        super.onPause();
+    }
 }
